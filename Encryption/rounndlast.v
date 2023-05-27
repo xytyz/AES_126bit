534318f7e3e0a3f7ff8018d20c653c004fb0b832
Kshitij Saxena <99387805+xytyz@users.noreply.github.com>
2023-05-28 00:34:57 +0530

    Add files via upload

--- Encryption/rounndlast.v
+++ Encryption/rounndlast.v
@@ -2,17 +2,17 @@
 
 module rounndlast(
     input clk,
-    input [3:0]rc,
+    input [3:0]r_count,
     input [127:0]rin,
     input [127:0]keylastin,
     output [127:0]fout
 );
 
-wire [127:0] sb,sr,mcl,keyout;
+wire [127:0] subbed,shifted,mixed,keyout;
 
-KeyGeneration t0(rc,keylastin,keyout);
-subbytes t1(rin,sb);
-shiftrow t2(sb,sr);
-assign fout= keyout^sr;
+KeyGeneration t0(r_count,keylastin,keyout);
+subbytes t1(rin,subbed);
+shiftrow t2(subbed,shifted);
+assign fout= keyout^shifted;
 
 endmodule
